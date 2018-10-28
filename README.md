3736a7880928cf4d39d6ec06c07b546808de1323
S Daron <simon@surlaterre.org>
2018-10-28 07:31:27 +0100

    scripts

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-#test
-test site
+#Metascript
+
+Kit de démarrage pour metalsmith.
+
+http://www.metalsmith.io/
+
+Cloner et anonymiser
+~~~~ 
+git clone git@github.com:SDaron/metalscript.git
+bash gitrm.sh
+~~~~ 
+
+Pour initialiser le dépot
+~~~~ 
+bash gitinit.sh
+~~~~ 
--- gitinit.sh
+++ gitinit.sh
@@ -5,6 +5,12 @@ git init
 git add .
 git commit -m "Initial commit"
 echo Hello, who am I talking to?
-read -p 'Adresse du dépot: (ex: git@github.com:user/project.git)' depot
-git remote add origin $depot
-git push -u origin master
+
+read -p "Do you want to add a remote origin?" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  read -p 'Repository URL: (ex: git@github.com:user/project.git)' url
+  git remote add origin $url
+  git push -u origin master
+fi
