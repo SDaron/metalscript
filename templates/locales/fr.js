9b2406da40ad1e9867265a59c193137ac44e4ce2
S Daron <simon@surlaterre.org>
2019-03-16 17:09:49 +0100

    Doublons

--- templates/locales/fr.js
+++ templates/locales/fr.js
@@ -4,7 +4,6 @@
 	"weekend": "week-end",
 	"Hello %s, how are you today? How was your %s.": "Bonjour %s, comment allez-vous aujourd'hui ? Comment était votre %s.",
 	"Hi": "Salut",
-	"Howdy": "Salut",
 	"%s cat": {
 		"one": "%s chat",
 		"other": "%s chats"
@@ -13,11 +12,5 @@
 		"one": "Il y a un singe sur l'%%s",
 		"other": "Il y a %d singes sur l'%%s"
 	},
-	"tree": "arbre",
-	"There is one monkey in the %s": {
-		"one": "There is one monkey in the %s",
-		"other": "There are %d monkeys in the %s"
-	},
-	"Hello %s": "Bonjour %s",
-	"hello": "hello"
+	"tree": "arbre"
 }
